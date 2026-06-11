145fb0d70a75c6210de647bcab6750b64c765aaf
Muraya-Pat <infoneygamedevelopers@gmail.com>
2026-06-11 21:49:32 +0300

    added todo modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TodosModule } from './todos/todos.module';
 
 
 @Module({
@@ -26,6 +27,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    TodosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
